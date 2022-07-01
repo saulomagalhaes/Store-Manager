aa09f21234d3aff770611648cf54f3bcd99457a3
Saulo Magalhães <saulo.magalhaes@outlook.com.br>
2022-07-01 15:24:13 -0300

    test(salesService): implementação de testes unitários para a função addSale

---
+++ tests/unit/services/salesServices.test.js
@@ -0,0 +1,43 @@
+const chai = require("chai");
+const chaiAsPromised = require("chai-as-promised");
+const sinon = require("sinon");
+chai.use(chaiAsPromised);
+
+const { salesService } = require("../../../services/salesService");
+const { salesModel } = require("../../../models/salesModel");
+
+describe("SalesServices", () => {
+  beforeEach(() => {
+    sinon.restore();
+  });
+
+  describe("#addSale", () => {
+    it("deve disparar um erro caso o salesModel dispare um erro", () => {
+      sinon.stub(salesModel, "existsProduct").rejects();
+      return chai.expect(salesService.addSale([{}, {}])).to.eventually.rejected;
+    });
+
+    it("deve disparar um erro caso o salesModel retorne um array vazio", () => {
+      sinon.stub(salesModel, "existsProduct").resolves([]);
+      return chai
+        .expect(salesService.addSale([{}, {}]))
+        .to.eventually.be.rejectedWith("Product not found");
+    });
+
+    it("deve disparar um erro caso o salesModel retorne um array com tamanho diferente", () => {
+      sinon.stub(salesModel, "existsProduct").resolves([{}]);
+      return chai
+        .expect(salesService.addSale([{}, {}]))
+        .to.eventually.be.rejectedWith("Product not found");
+    });
+
+    it("deve retornar um objeto caso esteja tudo ok", () => {
+      sinon.stub(salesModel, "existsProduct").resolves([{}, {}]);
+      sinon.stub(salesModel, "addSale").resolves(1);
+      sinon.stub(salesModel, "addSalesProducts").resolves();
+      return chai
+        .expect(salesService.addSale([{}, {}]))
+        .to.eventually.deep.equal({ id: 1, itemsSold: [{}, {}] });
+    });
+  });
+});
