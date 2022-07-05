39d483769f31835cb1f2ca7c4974e28b39d4cd31
Saulo Magalhães <saulo.magalhaes@outlook.com.br>
2022-07-05 16:24:32 -0300

    feat(github): subindo projeito feito na trybe para validação dos conhecimentos para repositório pessoal

--- app.js
+++ app.js
@@ -10,18 +10,10 @@ const app = express();
 
 app.use(express.json());
 
-// não remova esse endpoint, é para o avaliador funcionar
-app.get('/', (_request, response) => {
-  response.send();
-});
-
 app.use(router);
 
 app.use(errorHandler);
 
 app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
-// não remova essa exportação, é para o avaliador funcionar
-// você pode registrar suas rotas normalmente, como o exemplo acima
-// você deve usar o arquivo index.js para executar sua aplicação
 module.exports = app;
